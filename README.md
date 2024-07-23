7a086c42df96f8e4cc27c9cef8fe54c64591e8bc
Anirudh Rajhgopalann <anirudhsr97@gmail.com>
2024-07-23 20:48:34 +0530

    changes

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/HfhAxLC5)
-[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-2e0aaae1b6195c2367325f4f02e2d04e9abb55f0b24a779b69b11b9e10269abc.svg)](https://classroom.github.com/online_ide?assignment_repo_id=15449298&assignment_repo_type=AssignmentRepo)
+# DocStrings
+# Annotations
+
+DocStrings are string literals that appear right after the definition of a function, method, class, or module. They provide a convenient way to associate documentation with Python modules, functions, classes, and methods. Function annotations, introduced in Python 3, offer a way to attach metadata to function parameters and return values.
+
+# Lambda Expressions
+
+Lambda expressions in Python are small anonymous functions that can have any number of arguments but can only have one expression. They are useful for creating short, one-time-use functions without formally defining them using the def keyword.
+
+# Lambdas and Sorting
+
+Lambda functions are often used in sorting operations, particularly with the sort() method or sorted() function. They provide a quick way to specify custom sorting criteria for complex data structures like lists of tuples or dictionaries.
+
+# Function Introspection
+
+Function introspection refers to the ability to examine attributes about objects like functions at runtime. Python provides several built-in functions and attributes for introspection, such as dir(), type(), id(), __name__, and __doc__.
+
+# Callables
+
+In Python, a callable is any object that can be called like a function. This includes user-defined functions, built-in functions, methods of classes, class objects, and instances of classes that implement the __call__ method.
+
+# Map, Filter and Zip
+
+map(): Applies a given function to each item in an iterable and returns an iterator of results.
+filter(): Creates an iterator from elements of an iterable for which a function returns True.
+zip(): Creates an iterator of tuples where each tuple contains the i-th element from each of the input iterables.
+
+These functions are key components in functional programming, allowing for efficient data manipulation without explicit loops.
+
+# Reducing Functions
+
+Reducing functions, like reduce() from the functools module, apply a function of two arguments cumulatively to the items of a sequence, reducing it to a single value. This is useful for performing computations on lists where you want to combine elements.
+
+# Partial Functions
+
+Partial functions, created using functools.partial(), allow you to fix a certain number of arguments of a function and generate a new function. This is useful when you want to create a new function with some preset parameters from an existing function.
+
+# Operator Module
+
+The operator module provides a set of efficient functions corresponding to the intrinsic operators of Python. It's particularly useful in functional programming contexts, offering alternatives to lambda functions for common operations.
+These concepts form the foundation of functional programming in Python, enabling more concise, readable, and often more efficient code. By mastering these techniques, developers can write more elegant and powerful Python programs.
+
+time_it(fn, *args, repetitions= 1, **kwargs)
+This is a genralized function to call any function user specified number of times and return the average time taken for calls
+
+squared_power_list
+Retruns list by raising number to power from start to end -> number**start to number**end. Default start is 0 and end is 5"
+
+polygon_area
+Returns area of a regular polygon with number of sides between 3 to 6 bith inclusive
+
+temp_converter 
+Converts temprature from celsius 'c' to fahrenheit 'f' or fahrenheit to celsius
+
+speed_converter
+Converts speed from kmph (provided by user as input) to different units dist can be km/m/ft/yrd time can be ms/s/min/hr/day
---
+++ session5.py
@@ -0,0 +1,125 @@
+import time
+import math
+
+def time_it(fn, *args, repetitions=1, **kwargs):
+    """This is a generalized function to call any function user
+    specified number of times and return the average time taken for calls"""
+    if repetitions < 0:
+        return "Repetitions cannot be negative."
+    elif repetitions == 0:
+        return 0
+    elif not callable(fn):
+        raise TypeError("required positional argument: 'fn'")
+    ls = []
+    for _ in range(repetitions):
+        start = time.time()
+        fn(*args, **kwargs)
+        end = time.time()
+        ls.append(end-start)
+    return (sum(ls) / repetitions)
+
+def squared_power_list(number, *args, start=0, end=5, **kwargs):
+    """Returns list by raising number to power from start to end
+    -> number**start to number**end. Default start is 0 and end is 5"""
+    if args:
+        raise TypeError("squared_power_list function takes maximum 1 positional arguments, more provided")
+    if len(kwargs) > 0:
+        raise TypeError("squared_power_list function takes maximum 2 keyword/named arguments, more provided")
+    if not isinstance(number, int):
+        raise TypeError("Only integer type arguments are allowed")
+    if start < 0 or end < 0:
+        raise ValueError("Value of start or end can't be negative")
+    if end < start:
+        raise ValueError("Value of start should be less than end")
+    if number >= 10:
+        raise ValueError("Value of number should be less than 10")
+    return [number ** x for x in range(start, end+1)]
+
+def polygon_area(length, *args, sides=3, **kwargs):
+    """Returns area of a regular polygon with number of sides between
+    3 to 6 both inclusive"""
+    if args:
+        raise TypeError("polygon_area function takes maximum 1 positional arguments, more provided")
+    if len(kwargs) > 0:
+        raise TypeError("polygon_area function takes maximum 1 keyword/named arguments, more provided")
+    if not isinstance(length, int):
+        raise ValueError("Length can only be an integer")
+    if not isinstance(sides, int):
+        raise ValueError("Number of sides can only be an integer")
+    if length <= 0:
+        raise TypeError("Length should always be > 0")
+    if sides < 3 or sides > 6:
+        raise TypeError("Number of sides must be between 3 and 6")
+    area = (sides * length**2) / (4 * math.tan(math.pi / sides))
+    return area
+
+def temp_converter(temp, *args, temp_given_in='f', **kwargs):
+    """Converts temperature from celsius 'c' to fahrenheit 'f' or
+    fahrenheit to celsius"""
+    if args:
+        raise TypeError("temp_converter function takes maximum 1 positional arguments, more provided")
+    if len(kwargs) > 0:
+        raise TypeError("temp_converter function takes maximum 1 keyword/named arguments, more provided")
+    if not isinstance(temp, (int, float)):
+        raise ValueError("temp can only be a float")
+    if not isinstance(temp_given_in, str):
+        raise TypeError("Character string expected")
+    temp_given_in = temp_given_in.lower()
+    if temp_given_in not in ['f', 'c']:
+        raise ValueError("Only f or c is allowed")
+    if temp_given_in == 'c':
+        if temp < -273.15:
+            raise ValueError("Temperature can't go below -273.15 celsius = 0 Kelvin")
+        return (temp * 9/5) + 32
+    else:  # temp_given_in == 'f'
+        if temp < -459.67:
+            raise ValueError("Temperature can't go below -459.67 fahrenheit = 0 Kelvin")
+        return (temp - 32) * 5/9
+
+def speed_converter(speed, *args, dist='km', time='min', **kwargs):
+    """Converts speed from kmph (provided by user as input) to different units
+    dist can be km/m/ft/yrd time can be ms/s/min/hr/day """
+    if args:
+        raise TypeError("speed_converter function takes maximum 1 positional arguments, more provided")
+    if len(kwargs) > 0:
+        raise TypeError("speed_converter function takes maximum 2 keyword/named arguments, more provided")
+    if not isinstance(speed, (int, float)):
+        raise TypeError("Speed can be int or float type only")
+    if speed < 0:
+        raise ValueError("Speed can't be negative")
+    if speed > 300000:
+        raise ValueError("Speed can't be greater than speed of light")
+    if not isinstance(dist, str):
+        raise TypeError("Character string expected for distance unit")
+    if not isinstance(time, str):
+        raise TypeError("Character string expected for time unit")
+    
+    dist = dist.lower()
+    time = time.lower()
+    
+    if dist not in ['km', 'm', 'ft', 'yrd']:
+        raise ValueError("Incorrect unit of distance. Only km/m/ft/yrd allowed")
+    if time not in ['ms', 's', 'min', 'hr', 'day']:
+        raise ValueError("Incorrect unit of Time. Only ms/s/min/hr/day allowed")
+    
+    # Convert speed to m/s
+    if dist == 'km':
+        speed_ms = speed * 1000 / 3600
+    elif dist == 'm':
+        speed_ms = speed / 3600
+    elif dist == 'ft':
+        speed_ms = speed * 0.3048 / 3600
+    else:  # yrd
+        speed_ms = speed * 0.9144 / 3600
+    
+    # Convert to desired time unit
+    if time == 'ms':
+        return speed_ms * 1000
+    elif time == 's':
+        return speed_ms
+    elif time == 'min':
+        return speed_ms * 60
+    elif time == 'hr':
+        return speed_ms * 3600
+    else:  # day
+        return speed_ms * 86400
